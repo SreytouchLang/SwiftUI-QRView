745d000f3beb3fbe4855c0f4f47f1e75c8c2bdbe
SreytouchLang <atouch.it@gmail.com>
2024-02-23 15:21:25 -0800

    push code

---
+++ SwiftUI-QRView SwiftUI-QRView SwiftUI-QRView/ContentView.swift
@@ -0,0 +1,18 @@
+//
+//  ContentView.swift
+//  SwiftUI-QRView SwiftUI-QRView SwiftUI-QRView
+//
+//  Created by Sreytouch(Jessica) on 2/23/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        QRView()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SwiftUI-QRView SwiftUI-QRView SwiftUI-QRView/SwiftUI_QRView_SwiftUI_QRView_SwiftUI_QRViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_QRView_SwiftUI_QRView_SwiftUI_QRViewApp.swift
+//  SwiftUI-QRView SwiftUI-QRView SwiftUI-QRView
+//
+//  Created by Sreytouch(Jessica) on 2/23/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_QRView_SwiftUI_QRView_SwiftUI_QRViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
